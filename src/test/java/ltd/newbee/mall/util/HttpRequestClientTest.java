f6a116702badaa3302b96045079b1dec6a7c9650
taojigu <taojigu@163.com>
2024-11-07 11:59:48 +1300

    add HttpClient and test cases

---
+++ src/test/java/ltd/newbee/mall/util/HttpRequestClientTest.java
@@ -0,0 +1,37 @@
+package ltd.newbee.mall.util;
+
+import ltd.newbee.mall.NewBeeMallAPIApplication;
+import ltd.newbee.mall.api.mall.vo.NewBeeMallIndexCategoryVO;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
+import test.HttpRequestClient;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest(classes = NewBeeMallAPIApplication.class,webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HttpRequestClientTest {
+    @LocalServerPort
+    int port;
+    final String host = "localhost";
+    HttpRequestClient client;
+    @BeforeEach
+    public void eachSetup() {
+        if (null == client) {
+            client = new HttpRequestClient(host, port);
+        }
+    }
+    @Test
+    public void testGetMethod() {
+        String path = "/ltd/newbee/mall/api/v1/categories";
+        ResponseEntity<Result<List<NewBeeMallIndexCategoryVO>>> response =
+                client.get(path,null);
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+    }
+}
