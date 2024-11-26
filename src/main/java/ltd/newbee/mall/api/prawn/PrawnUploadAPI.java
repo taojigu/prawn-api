2076181c3b5b1a76b6be53191423981faff4aa2d
taojigu <taojigu@163.com>
2024-11-27 12:58:19 +1300

    remove useless uploading image code and dependency

--- src/main/java/ltd/newbee/mall/api/prawn/PrawnUploadAPI.java
+++ src/main/java/ltd/newbee/mall/api/prawn/PrawnUploadAPI.java
@@ -1,22 +1,13 @@
 package ltd.newbee.mall.api.prawn;
 
-import cn.hutool.core.util.StrUtil;
-import com.qcloud.cos.COSClient;
-import com.qcloud.cos.ClientConfig;
-import com.qcloud.cos.auth.BasicCOSCredentials;
-import com.qcloud.cos.auth.COSCredentials;
-import com.qcloud.cos.model.CannedAccessControlList;
-import com.qcloud.cos.model.PutObjectRequest;
-import com.qcloud.cos.region.Region;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import ltd.newbee.mall.common.Constants;
 import ltd.newbee.mall.util.*;
 import ltd.prawn.config.annotation.TokenToPrawnUser;
 import ltd.prawn.entity.PrawnUserEntity;
-import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -51,7 +42,7 @@ public class PrawnUploadAPI {
         }
 
         String filePath = file.getOriginalFilename();
-        String key = "product_"+getFileKey(filePath);
+        String key = "product_image/"+getFileKey(filePath);
         String bucketName = "prawn-image-bucket1";
         String imageUrl = S3ImageUploader.uploadImageToS3(bucketName,file,key);
         if (StringUtils.hasLength(imageUrl)){
@@ -67,109 +58,41 @@ public class PrawnUploadAPI {
      * @return
      */
     private String getFileKey(String originalfileName) {
-        String filePath = "test/";
+        String filePath = "";
         //1.获取后缀名 2.去除文件后缀 替换所有特殊字符
-        String fileType = originalfileName.substring(originalfileName.lastIndexOf("."));
-        String fileStr = StrUtil.removeSuffix(originalfileName, fileType).replaceAll("[^0-9a-zA-Z\\u4e00-\\u9fa5]", "_");
-        filePath += new DateTime().toString("yyyyMMddHHmmss") + "_" + fileStr + fileType;
+        String fileName = processFileName(originalfileName);
+        filePath += UUID.randomUUID()+ "_" + fileName;
         return filePath;
     }
 
-    /**
-     * 图片上传
-     */
-    @RequestMapping(value = "prawn/upload/file", method = RequestMethod.POST)
-    @ApiOperation(value = "单图上传", notes = "file Name \"file\"")
-    public Result upload(HttpServletRequest httpServletRequest, @RequestParam("file") MultipartFile file, @TokenToPrawnUser PrawnUserEntity prawnUserEntity) throws URISyntaxException {
 
-        String fileName = file.getOriginalFilename();
-        int suffIndex = fileName.lastIndexOf(".");
-        String suffixName = ".jpg";
-        if (-1 < suffIndex) {
-            suffixName = fileName.substring(fileName.lastIndexOf("."));
-        }
-        //生成文件名称通用方法
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
-        Random r = new Random();
-        StringBuilder tempName = new StringBuilder();
-        tempName.append(sdf.format(new Date())).append(r.nextInt(100)).append(suffixName);
-        String newFileName = tempName.toString();
-        File fileDirectory = new File(Constants.FILE_UPLOAD_DIC);
-        //创建文件
-        File destFile = new File(Constants.FILE_UPLOAD_DIC + newFileName);
-        try {
-            if (!fileDirectory.exists()) {
-                if (!fileDirectory.mkdir()) {
-                    throw new IOException("文件夹创建失败,路径为：" + fileDirectory);
-                }
-            }
-            file.transferTo(destFile);
-            Result resultSuccess = ResultGenerator.genSuccessResult();
-            String targetURL = Constants.getImageURL(newFileName);
-            resultSuccess.setData(targetURL);
-            // resultSuccess.setData(NewBeeMallUtils.getHost(new URI(httpServletRequest.getRequestURL() + "")) + "/upload/" + newFileName);
-            return resultSuccess;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return ResultGenerator.genFailResult("文件上传失败");
-        }
+    /**
+     * 处理文件名：去除后缀并替换特殊字符
+     *
+     * @param fileName 原始文件名
+     * @return 处理后的文件名
+     */
+    public static String processFileName(String fileName) {
+        // 1. 去除后缀
+        String nameWithoutExtension = removeFileExtension(fileName);
+
+        // 2. 替换所有特殊字符
+        String sanitizedFileName = nameWithoutExtension.replaceAll("[^a-zA-Z0-9_-]", "_");
+
+        return sanitizedFileName;
     }
 
     /**
-     * 图片上传
+     * 去除文件的扩展名
+     *
+     * @param fileName 文件名
+     * @return 没有扩展名的文件名
      */
-    @RequestMapping(value = "prawn/upload/files", method = RequestMethod.POST)
-    @ApiOperation(value = "多图上传", notes = "wangEditor图片上传")
-    public Result uploadV2(HttpServletRequest httpServletRequest, @TokenToPrawnUser PrawnUserEntity prawnUserEntity) throws URISyntaxException {
-
-        List<MultipartFile> multipartFiles = new ArrayList<>(8);
-        if (standardServletMultipartResolver.isMultipart(httpServletRequest)) {
-            MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) httpServletRequest;
-            Iterator<String> iter = multiRequest.getFileNames();
-            int total = 0;
-            while (iter.hasNext()) {
-                if (total > 5) {
-                    return ResultGenerator.genFailResult("最多上传5张图片");
-                }
-                total += 1;
-                MultipartFile file = multiRequest.getFile(iter.next());
-                multipartFiles.add(file);
-            }
+    public static String removeFileExtension(String fileName) {
+        int lastDotIndex = fileName.lastIndexOf(".");
+        if (lastDotIndex > 0) {
+            return fileName.substring(0, lastDotIndex);
         }
-        if (CollectionUtils.isEmpty(multipartFiles)) {
-            return ResultGenerator.genFailResult("参数异常");
-        }
-        if (multipartFiles != null && multipartFiles.size() > 5) {
-            return ResultGenerator.genFailResult("最多上传5张图片");
-        }
-        List<String> fileNames = new ArrayList(multipartFiles.size());
-        for (int i = 0; i < multipartFiles.size(); i++) {
-            String fileName = multipartFiles.get(i).getOriginalFilename();
-            String suffixName = fileName.substring(fileName.lastIndexOf("."));
-            //生成文件名称通用方法
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
-            Random r = new Random();
-            StringBuilder tempName = new StringBuilder();
-            tempName.append(sdf.format(new Date())).append(r.nextInt(100)).append(suffixName);
-            String newFileName = tempName.toString();
-            File fileDirectory = new File(Constants.FILE_UPLOAD_DIC);
-            //创建文件
-            File destFile = new File(Constants.FILE_UPLOAD_DIC + newFileName);
-            try {
-                if (!fileDirectory.exists()) {
-                    if (!fileDirectory.mkdir()) {
-                        throw new IOException("文件夹创建失败,路径为：" + fileDirectory);
-                    }
-                }
-                multipartFiles.get(i).transferTo(destFile);
-                fileNames.add(NewBeeMallUtils.getHost(new URI(httpServletRequest.getRequestURL() + "")) + "/upload/" + newFileName);
-            } catch (IOException e) {
-                e.printStackTrace();
-                return ResultGenerator.genFailResult("文件上传失败");
-            }
-        }
-        Result resultSuccess = ResultGenerator.genSuccessResult();
-        resultSuccess.setData(fileNames);
-        return resultSuccess;
+        return fileName; // 如果没有后缀，返回原文件名
     }
 }
