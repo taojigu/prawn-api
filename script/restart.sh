713442ef10b895470ad499c9c89e41171482cee5
taojigu <taojigu@163.com>
2024-11-27 13:54:25 +1300

    add profile in restart script

--- script/restart.sh
+++ script/restart.sh
@@ -1,9 +1,9 @@
-
+environment=${1:-dev}
 APP_NAME=newbee-mall-api-3.0.0-SNAPSHOT
 
 
 pkill -f $APP_NAME.jar
 # 指定配置文件，启动Jar，控制台默认输出到nohup.out文件
 #
-nohup java -jar target/$APP_NAME.jar   &
+nohup java  -Dspring.profiles.active=$environment -jar target/$APP_NAME.jar   &
 
